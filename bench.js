0bb95a6364e08921775bf88aa95b6038d751935d
ayushmishra2005 <ayushmishra2005@gmail.com>
2021-05-26 08:31:09 +0530

    refactopring

--- bench.js
+++ bench.js
@@ -359,6 +359,8 @@ async function benchmarkRuntime(app, config) {
         var error = prepareBranch(config, { benchContext })
         if (error) return error
         console.log(`branchCommand is  "${branchCommand}."`);
+
+        shell.cd(cwd + `/git/${config.repo}`);
         var { error, stdout, stderr } = benchContext.runTask(branchCommand, `Benching branch: ${config.branch}...`);
 
         // If `--output` is set, we commit the benchmark file to the repo
